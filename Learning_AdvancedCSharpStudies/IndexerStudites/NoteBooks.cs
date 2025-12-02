b354a84fad659061eb694e03fb064ad50a0254ef
Mike Schnobrich <snowyriversoftware@outlook.com>
2025-12-02 14:53:51 -0700

    Add indexer implementation and unit tests for NoteBooks class

---
+++ Learning_AdvancedCSharpStudies/IndexerStudites/NoteBooks.cs
@@ -0,0 +1,12 @@
+﻿namespace Learning_AdvancedCSharpStudies.IndexerStudites;
+
+public class NoteBooks
+{
+    private readonly string[] _notebooks = new string[1000];
+
+    public string this[int index]
+    {
+        get => _notebooks[index];
+        set => _notebooks[index] = value;
+    }
+}
---
+++ Tests_LearningAdvancedCSharpStudies/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Learning_AdvancedCSharpStudies.IndexerStudites;
+
+namespace Tests_LearningAdvancedCSharpStudies;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var noteBooks = new NoteBooks
+        {
+            [0] = "Introduction to C#",
+            [1] = "Advanced C# Concepts"
+        };
+
+        // Check using fluent assertions if the notes are correctly set
+        noteBooks[0].Should().Be("Introduction to C#");
+    }
+}
